51cb1f7aba9ad19e351c18d9b2f091ccf31e4f6a
elakscoder <celakkiyaece@gmail.com>
2024-07-22 06:06:31 +0530

    maximum number of moves queen can make

--- c/array/maximum_moves_queen_can_make.c
+++ c/array/maximum_moves_queen_can_make.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 
+int obstacles(int x,int y,int k,int obs[])
+{
+    for(int i=0;i<k*2;i+=2)
+    {
+        if(x==obs[i] && y == obs[i+1])
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
 int range(int n,int x,int y)
 {
     if(x>0 && x<=n && y>0 && y<=n)
@@ -11,40 +24,31 @@ int range(int n,int x,int y)
 }
 
 
-int moves(int n,int x,int y,int posx,int posy,int k,int kx,int ky)
+int moves(int n,int x,int y,int posx,int posy,int k,int obs[])
 {
     int cnt = 0;
 
-    while(range(n,x,y) && !(x == kx && y == ky))
+    while(range(n,x,y) && obstacles(x,y,k,obs))
     {
         x += posx;
         y += posy;
         cnt++;
     }
-    printf("count: %d\n",cnt);
     return cnt-1;
 }
 
-int cells(int n,int x,int y,int k,int kx,int ky)
+int cells(int n,int x,int y,int k,int obs[])
 {
     int ans = 0;
 
-    ans += moves(n,x,y,0,-1,k,kx,ky);
-    printf("%d ",ans);
-    ans += moves(n,x,y,1,-1,k,kx,ky);
-    printf("%d ",ans);
-    ans += moves(n,x,y,1,0,k,kx,ky);
-    printf("%d ",ans);
-    ans += moves(n,x,y,1,1,k,kx,ky);
-    printf("%d ",ans);
-    ans += moves(n,x,y,0,1,k,kx,ky);
-    printf("%d ",ans);
-    ans += moves(n,x,y,-1,1,k,kx,ky);
-    printf("%d ",ans);
-    ans += moves(n,x,y,-1,0,k,kx,ky);
-    printf("%d ",ans);
-    ans += moves(n,x,y,-1,-1,k,kx,ky);
-    printf("%d ",ans);
+    ans += moves(n,x,y,0,-1,k,obs);
+    ans += moves(n,x,y,1,-1,k,obs);
+    ans += moves(n,x,y,1,0,k,obs);
+    ans += moves(n,x,y,1,1,k,obs);
+    ans += moves(n,x,y,0,1,k,obs);
+    ans += moves(n,x,y,-1,1,k,obs);
+    ans += moves(n,x,y,-1,0,k,obs);
+    ans += moves(n,x,y,-1,-1,k,obs);
 
     return ans;
 }
@@ -58,9 +62,6 @@ int main()
 
     int k;
 
-    /* int kx;
-    int ky; */
-
     printf("Enter n: ");
     scanf("%d",&n);
 
@@ -73,17 +74,23 @@ int main()
     printf("Enter the number of obstacles: ");
     scanf("%d",&k);
 
-    for(int i=0;i<k;i++)
+    int obs[k];
+
+    for(int i=0;i<k*2;i++)
     {
-        
+        if(i%2)
+        {
+            printf("Enter the y%d position obstacle: ",(i/2)+1);
+            scanf("%d",&obs[i]);
+        }
+        else
+        {
+            printf("Enter the x%d position obstacle: ",(i/2)+1);
+            scanf("%d",&obs[i]);
+        }
     }
-    /* printf("Enter kx: ");
-    scanf("%d",&kx);
 
-    printf("Enter ky: ");
-    scanf("%d",&ky); */
-
-    printf("Output: %d",cells(n,x,y,k,kx,ky));
+    printf("Output: %d",cells(n,x,y,k,obs));
 
     return 0;
 
